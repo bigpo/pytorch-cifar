f01c940ec62e02d96942e2433fd5a154ab084362
songpo <songpo@momenta.ai>
2018-09-29 14:36:09 +0800

    update

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ import torch.backends.cudnn as cudnn
 
 import torchvision
 import torchvision.transforms as transforms
+from torchvision.models import resnet18
 
 import os
 import argparse
+from tensorboardX import SummaryWriter
 
 from models import *
 from utils import progress_bar
@@ -19,12 +21,15 @@ from utils import progress_bar
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--lr_decay', default=0.1, type=float, help='learning rate decay')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 args = parser.parse_args()
+os.chdir('/home/songpo/github_prj/pytorch-cifar')
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+writer = SummaryWriter('log')
 
 # Data
 print('==> Preparing data..')
@@ -52,6 +57,8 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 print('==> Building model..')
 # net = VGG('VGG19')
 net = ResNet18()
+# net = resnet18(pretrained=True) #from torchvision with pretrained
+# net.fc = nn.Linear(512, 10)
 # net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
@@ -62,7 +69,7 @@ net = ResNet18()
 # net = ShuffleNetG2()
 # net = SENet18()
 net = net.to(device)
-if device == 'cuda':
+if device == 'cuda:0':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 
@@ -81,6 +88,11 @@ optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5
 # Training
 def train(epoch):
     print('\nEpoch: %d' % epoch)
+
+    if epoch == 150 or epoch == 250:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] *= args.lr_decay
+
     net.train()
     train_loss = 0
     correct = 0
@@ -98,8 +110,14 @@ def train(epoch):
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
 
-        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-            % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        # progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+        #     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+        if batch_idx % 100 == 0:
+            writer.add_scalar('Train/Loss', loss.item(), epoch * len(trainloader) + batch_idx)
+
+    epoch_avg_loss = train_loss / len(trainloader)
+    epoch_avg_acc = 100. * correct / total
+    print('train loss: %f, train_acc: %f' % (epoch_avg_loss, epoch_avg_acc))
 
 def test(epoch):
     global best_acc
@@ -118,24 +136,28 @@ def test(epoch):
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            # progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+            #     % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+            if batch_idx % 100 == 0:
+                writer.add_scalar('Test/Accu', correct / total, epoch * len(testloader) + batch_idx)
+    epoch_avg_loss = test_loss / len(testloader)
+    epoch_avg_acc = 100. * correct / total
 
+    print('test loss: %f, test_acc: %f' % (epoch_avg_loss, epoch_avg_acc))
     # Save checkpoint.
-    acc = 100.*correct/total
-    if acc > best_acc:
+
+    if epoch_avg_acc > best_acc:
         print('Saving..')
         state = {
             'net': net.state_dict(),
-            'acc': acc,
+            'acc': epoch_avg_acc,
             'epoch': epoch,
         }
-        if not os.path.isdir('checkpoint'):
-            os.mkdir('checkpoint')
+        if not os.path.isdir('./checkpoint'):
+            os.mkdir('./checkpoint')
         torch.save(state, './checkpoint/ckpt.t7')
-        best_acc = acc
+        best_acc = epoch_avg_acc
 
-
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, start_epoch+350):
     train(epoch)
     test(epoch)
